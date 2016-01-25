f474f2849a6d0d5d97456cc0e56df0c522d8929d
minibear <lei.zhang.bupt08@gmail.com>
2016-01-25 17:13:05 +0800

    MOD:增加404页面

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/404', function(req, res, next) {
+  res.render('404');
+})
+
 module.exports = router;
