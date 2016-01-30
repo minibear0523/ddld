ca96f88cd364df21be25de32c39e242b6e56979a
minibear <lei.zhang.bupt08@gmail.com>
2016-01-30 17:34:37 +0800

    MOD:添加新闻列表模板

--- routes/news.js
+++ routes/news.js
@@ -14,31 +14,34 @@ var PAGE_LIMIT = 10;
  * :params kind: 资讯种类: company(公司动态), industry(行业资讯), 空(全部)
  * :params page: 分页的页数, 每页是PAGE_LIMIT条资讯.
  */
-router.get('/', function(req, res) {
-  var kind = req.query.kind;
-  var page = req.query.page || 1;
-  var skip = (page - 1) * PAGE_LIMIT;
-  var query = {};
-  if (kind) {
-    query['kind'] = kind;
-  }
-  News
-    .find(query)
-    .sort({date: -1})
-    .limit(PAGE_LIMIT)
-    .skip(skip)
-    .exec()
-    .then(function(news_list) {
-      var data = {
-        page: page,
-        kind: kind,
-        news_list: news_list
-      };
-      return res.render('news/list', data);
-    })
-    .catch(function(err) {
-      return res.status(404).send(err);
-    });
+//router.get('/', function(req, res) {
+//  var kind = req.query.kind;
+//  var page = req.query.page || 1;
+//  var skip = (page - 1) * PAGE_LIMIT;
+//  var query = {};
+//  if (kind) {
+//    query['kind'] = kind;
+//  }
+//  News
+//    .find(query)
+//    .sort({date: -1})
+//    .limit(PAGE_LIMIT)
+//    .skip(skip)
+//    .exec()
+//    .then(function(news_list) {
+//      var data = {
+//        page: page,
+//        kind: kind,
+//        news_list: news_list
+//      };
+//      return res.render('news/list', data);
+//    })
+//    .catch(function(err) {
+//      return res.status(404).send(err);
+//    });
+//});
+router.get('/', function(req, res, next) {
+  res.render('news');
 });
 
 /**
