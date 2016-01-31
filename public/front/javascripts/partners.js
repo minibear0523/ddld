de86a2d0317be6f8497956709cd5c2f16c0d9909
Jason Zhang <jzhang@skystarstudio.com>
2016-01-30 16:07:39 -0800

    MOD: enable baidu map

--- public/front/javascripts/partners.js
+++ public/front/javascripts/partners.js
@@ -1,7 +1,8 @@
 $(function() {
-  var map = new BMap.Map("#partners-places");
+  var map = new BMap.Map("partners-places");
   map.centerAndZoom(new BMap.Point(116.331205, 39.972179), 18);
-  map.addControll(new BMap.MapTypeControl());
+
   map.setCurrentCity('北京');
+  map.addControl(new BMap.MapTypeControl());
   map.enableScrollWheelZoom(true);
 });
