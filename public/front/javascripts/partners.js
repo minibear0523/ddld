bc7aae75da8ba8ec4e8f2c30c3093ebbeebf247a
minibear <lei.zhang.bupt08@gmail.com>
2016-01-31 18:28:15 +0800

    MOD:增加地图&联系我们

--- public/front/javascripts/partners.js
+++ public/front/javascripts/partners.js
@@ -1,8 +1,10 @@
 $(function() {
   var map = new BMap.Map("partners-places");
-  map.centerAndZoom(new BMap.Point(116.331205, 39.972179), 18);
+  map.centerAndZoom(new BMap.Point(106.05119,35.260616), 5);
 
   map.setCurrentCity('北京');
   map.addControl(new BMap.MapTypeControl());
   map.enableScrollWheelZoom(true);
+
+
 });
