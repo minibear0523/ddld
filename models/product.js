c44ef0f81ca40d53f78e239cdd704ace879b4bdb
minibear <lei.zhang.bupt08@gmail.com>
2016-02-14 21:01:37 +0800

    MOD:修改相关的model和view

--- models/product.js
+++ models/product.js
@@ -5,20 +5,24 @@ var Schema = mongoose.Schema;
  * 产品招商数据表
  * :name: 通用名
  * :name_co: 商用名(非必填)
- * :name_en: 英文名
- * :name_py: 汉语拼音
- * 其他使用JSON格式或者图片表示
+ * :detail: 直接字符串保存
+ * :kind: 招商项目还是转让项目, 其中转让项目还有附加的分类
+ * :sub_kind: 转让项目的附件分类
  */
 var Product = new Schema({
   // 药品名称
   name: String,
+  // 商品名
   name_co: {
     type: String,
     required: false
   },
-  name_en: String,
-  name_py: String,
-  detail: String
+  detail: String,
+  kind: String,
+  sub_kind: {
+    type: String,
+    required: false
+  }
 });
 
 module.exports = mongoose.model('Product', Product);
--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('products');
+router.get('transfer/', function(req, res, next) {
+  res.render('transfer');
+});
+
+router.get('merchant/', function(req, res, next) {
+  res.render('merchant');
 });
 
 module.exports = router;
