a4f5b7968c2792a0b3db8d4e69af70f723576561
minibear <lei.zhang.bupt08@gmail.com>
2016-03-07 21:57:24 +0800

    MOD:修改首页大图

--- public/front/javascripts/partners.js
+++ public/front/javascripts/partners.js
@@ -1,10 +1,17 @@
 $(function() {
   var map = new BMap.Map("partners-places");
   map.centerAndZoom(new BMap.Point(106.05119,35.260616), 5);
-
   map.setCurrentCity('北京');
   map.addControl(new BMap.MapTypeControl());
   map.enableScrollWheelZoom(true);
 
-
+  var customLayer;
+  function addCustomLayer(keyword) {
+    if (customLayer) {
+      map.removeTileLayer(customLayer);
+    }
+    customLayer = new BMap.CustomLayer({
+      
+    });
+  }
 });
