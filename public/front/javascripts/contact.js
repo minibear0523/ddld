3bd988ec4da5b4db82825819e1fb784c49e5d762
minibear <lei.zhang.bupt08@gmail.com>
2016-03-07 12:27:08 +0800

    MOD:

--- public/front/javascripts/contact.js
+++ public/front/javascripts/contact.js
@@ -1,6 +1,6 @@
 $(function() {
   var map = new BMap.Map('address-places');
-  map.centerAndZoom(new BMap.Point(106.05119, 35.260616), 5);
+  map.centerAndZoom(new BMap.Point(116.565021,39.781415), 5);
   map.setCurrentCity('北京');
   map.addControl(new BMap.MapTypeControl());
   map.enableScrollWheelZoom(true);
