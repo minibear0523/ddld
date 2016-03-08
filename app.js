99953903399cbdebda507a7e0c7746951dfb062f
minibear <lei.zhang.bupt08@gmail.com>
2016-03-08 16:42:41 +0800

    MOD:增加logger

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+// 使用express-bunyan-logger
+app.use(require('express-bunyan-logger')({
+    format: ":remote-address - :user-agent[major] custom logger"
+});
 app.use(compression());
 app.use(bodyParser.json({limit: '5MB'}));
 app.use(bodyParser.urlencoded({ extended: true, limit: '5MB' }));
