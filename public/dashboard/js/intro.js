09260087f7890553c1f6464736ad9819572879d5
minibear <lei.zhang.bupt08@gmail.com>
2016-04-28 15:34:38 +0800

    MOD:确保dashboard中可以删除image

--- public/dashboard/js/intro.js
+++ public/dashboard/js/intro.js
@@ -1,27 +1,29 @@
 (function($) {
   'use strict'
 
-  $('#delete-image-btn').on('click', function(e) {
-    e.preventDefault();
-    var url = e.target.parentElement.href;
-    var urlRegExp = new RegExp('\/certifications\/images\/([a-zA-Z0-9_.]*)');
-    var filename = urlRegExp.exec(url)[1];
-    var deleteURL = '/uploads/intro/' + filename;
-    Messenger().ajax({
-      successMessage: '删除成功',
-      errorMessage: '删除失败, 请重试',
-      progressMessage: '删除中...',
-      retry: false,
-      showCloseButton: false,
-      hideAfter: 3
-    }, {
-      url: deleteURL,
-      type: 'DELETE',
-      success: function() {
-        location.reload();
-      }
-    })
-  });
+  $(function() {
+    $('.delete-image-btn').on('click', function(e) {
+      e.preventDefault();
+      var url = e.target.parentElement.href;
+      var urlRegExp = new RegExp('\/certifications\/images\/([a-zA-Z0-9_.]*)');
+      var filename = urlRegExp.exec(url)[1];
+      var deleteURL = '/uploads/intro/' + filename;
+      Messenger().ajax({
+        successMessage: '删除成功',
+        errorMessage: '删除失败, 请重试',
+        progressMessage: '删除中...',
+        retry: false,
+        showCloseButton: false,
+        hideAfter: 3
+      }, {
+        url: deleteURL,
+        type: 'DELETE',
+        success: function() {
+          location.reload();
+        }
+      })
+    });
+  })
 
   /**
    * javascript-load-image插件相关函数
