d72c59224d6d63eac61c2890d9f120b968bcf3fd
minibear <lei.zhang.bupt08@gmail.com>
2016-05-06 18:07:09 +0800

    MOD:修改百度统计的代码, 从IP地址转到域名下

--- public/front/javascripts/baidu-analysis.js
+++ public/front/javascripts/baidu-analysis.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?16a561666b11915ed8f7a48d136979ea";
+  hm.src = "//hm.baidu.com/hm.js?3be730dfb8f2e3894d8af2fe2111afe6";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
