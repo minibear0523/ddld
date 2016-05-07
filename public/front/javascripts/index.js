8f51886d2d102a4280dbdb422cbbcbad2ff660c0
Jason Zhang <jzhang@skystarstudio.com>
2016-05-06 23:30:55 -0700

    MOD: update modal.js. modal.js has more powerful api, support multiple shortcut and support separate cookie for each modal now

--- public/front/javascripts/index.js
+++ public/front/javascripts/index.js
@@ -9,13 +9,75 @@
   /**
    * 增加搜索框 autofocus
    */
-   $('#search-modal').on('shown.bs.modal', function(e) {
-    console.log('yes');
-    $(this).find('input[type="text"]').focus();
-   });
+  $('#search-modal').on('shown.bs.modal', function(e) {
+   console.log('yes');
+   $(this).find('input[type="text"]').focus();
+  });
 
-   /*
-    * initialize modal in current page, details are in modal.js
-    */
-   initModal()
+  /*
+   * create modal in current page, see modal.js for details
+   */
+
+// option sample
+// {
+//   expiry: 2,
+//   icon: "envelope-o",
+//   'icon-text': "订阅我们"
+//   shortcut: {
+//     text: "<span>订阅</span><span>我们</span>"
+//     icon: "envelope-o" / false
+//     style: "cyanine"
+//   },
+//   confirm: {
+//     text: "订阅我们"
+//     method: 'POST'
+//     icon: "envelope-o" / false
+//   },
+//   autoShow: false
+// }
+
+// shortcut style options
+//   cyanine
+// , black-blue
+// , black-gloss
+// , black-satin
+// , blue-dark
+// , blue-gloss
+// , blue-ios
+// , blue-light
+// , gold
+// , green
+// , green-dark
+// , green-ios-gloss
+// , green-lime
+// , grey-light
+// , grey-light-alternate 
+// , grey-shiny
+// , khaki
+// , orange
+// , pink-gloss
+// , pink-hot
+// , purple
+// , red
+// , wood
+// , yellow
+
+  var customizedModal = ModalFactory.new('customized', '/modal_content', {
+    autoShow: true
+  });
+  var subscriptionModal = ModalFactory.new('subscription', '/front/data/subscription.json', {
+    icon: "envelope-o",
+    'btn-text': "订阅我们",
+    shortcut: {
+      text: "<span>订阅</span><span>我们</span>",
+      style: "cyanine"
+    },
+    confirm: {
+      method: 'POST'
+    }
+  });
+
+  // execute the deferred queue in ModalFactory, see modal.js for details
+  ModalFactory.show()
+
 }(jQuery));
